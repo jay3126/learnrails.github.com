6c7153d743f09d06927202420b0b276ad25992ee
Daniel Kehoe <daniel@danielkehoe.com>
2013-08-02 17:51:12 +0000

    publish initial mockup of structure

---
+++ publish.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo "publishing to the learnrails.github.com site"
+cd ~/workspace/learnrails/learnrails.github.com.wiki
+gollum-site generate
+echo "GENERATED";
+cp -R _site/* ../learnrails.github.com
+rm -rf _site/
+cd ../learnrails.github.com
+mv Home.html index.html
+# echo "COMMITTING";
+# git add .
+# git commit -am "update website"
+# git push origin master
+echo "FINISHED";
+exit 0
